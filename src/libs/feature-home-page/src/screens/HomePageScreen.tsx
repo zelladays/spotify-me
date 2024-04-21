7ecfa366e4651ad23b4d0b3fbde81308e7578de7
zelladays <lj_west@hotmail.com>
2024-04-21 23:14:12 +0100

    feat: title styling change

--- src/libs/feature-home-page/src/screens/HomePageScreen.tsx
+++ src/libs/feature-home-page/src/screens/HomePageScreen.tsx
@@ -2,11 +2,18 @@ import { Button, Flex, Text } from "@chakra-ui/react";
 import { useTheme } from "../../../shared/theme";
 
 export const HomePageScreen = () => {
-  const { textStyles } = useTheme();
+  const { textStyles, colors } = useTheme();
   return (
     <Flex flexDirection="column" p="16" gap="8">
       <Flex justifyContent="space-between">
-        <Text {...textStyles.h1}>SpotifyMe.</Text>
+        <Text {...textStyles.h1}>
+          <Text as="span" color={colors.brandGreen}>
+            Spotify
+          </Text>
+          <Text as="span" color={colors.textPrimary}>
+            Me.
+          </Text>
+        </Text>
         <Button>Log Out</Button>
       </Flex>
       <Flex gap="8">
